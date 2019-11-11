0983a2e4a038abeea1926041a35a70eb88a94211
DimaGavrushko <gavrushko98@gmail.com>
2019-11-11 16:20:59 +0300

    add database

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+/*mongoose.connect(`mongodb+srv://admin:admin@cluster0-jnotm.mongodb.net/test?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Successfully connected to database"));*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-jnotm.mongodb.net/test?retryWrites=true&w=majority`;
+MongoClient.connect(uri, function(err, client) {
+    if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    }
+    console.log('Connected...');
+    const collection = client.db(process.env.DB).collection("posts");
+    console.log(collection);
+    // perform actions on the collection object
+    client.close();
+});
 
 app.get('/', (req, res) => {
     res.send("Hello world");
