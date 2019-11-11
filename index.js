5ff835838d2de99a98db3ca5a240c4b4f16b5bee
Dmitry <gavrushko98@gmail.com>
2019-11-11 23:08:59 +0300

    send post in response, refactor deploy script

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const config = require("./config");
+const schema = new mongoose.Schema({name: String, id: Number});
+const Post = mongoose.model('Post', schema);
 
 const uri = `mongodb+srv://${config.dbUser}:${config.dbUserPassword}@cluster0-jnotm.mongodb.net/${config.dbName}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
-    console.log("Successfully connected to database");
-
-    // Example of using mongoose
-    /*const schema = new mongoose.Schema({name: String, id: Number});
-    const Post = mongoose.model('Post', schema);
-    Post.find({}).then( res => console.log(res))*/
-});
+}).then(() => console.log("Successfully connected to database"));
 
 
 app.get('/', (req, res) => {
-    res.send("Hello world.");
+    Post.find({}).then(result => res.json({ posts: result}));
 });
+
 app.listen(config.port, () => {
     console.log("Server is started")
 });
